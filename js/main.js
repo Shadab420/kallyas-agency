c4e83d2e6c122e3fac86e2cbcbea0232bc73aab6
Shadab420 <shadab_anwar@outlook.com>
2020-07-29 08:01:19 +0600

    added smooth scroll

--- js/main.js
+++ js/main.js
@@ -28,8 +28,38 @@ $(document).ready(function(){
     //set footer date
     $('#date').html(new Date().getFullYear());
 
+    //menu background change on scroll
+    $(window).scroll(() => {
+        let fromTopPx = 300; // distance to trigger
+        let scrolledFromtop = jQuery(window).scrollTop();
+
+        if(scrolledFromtop > fromTopPx){
+            $('#header').css({ backgroundColor: 'rgba(250, 81, 88, 0.9)'});
+        }else{
+            $('#header').css({ backgroundColor: 'transparent'});
+        }
+    });
+
     //menu toggle click handler
     $('#menu-toggle').click(() => {
-        $('.nav .nav-items').css({ display: 'flex'});
+        let visible = $('.nav .nav-items').css("display");
+
+       if(visible === 'none') $('.nav .nav-items').css({ display: 'flex'});
+       else $('.nav .nav-items').css({ display: 'none'});
     })
+
+    // smoot scrol for IE, EDGE, Safary
+	$("a").on('click', function(event){
+		if(this.hash !== ""){
+			event.preventDefault();
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+				window.location.hash = hash; 
+			})
+		}
+	});
+ 
 })
